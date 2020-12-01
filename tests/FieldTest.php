6a1c79da0c93eb648e808acbf03772476ff2715f
Nikolay Gorohovsky <fi1a@icloud.com>
2020-12-01 19:31:53 +0500

    Тестирование метода add класса Field

--- tests/FieldTest.php
+++ tests/FieldTest.php
@@ -6,6 +6,8 @@ namespace Fi1a\Unit\UserSettings;
 
 use Fi1a\Unit\UserSettings\TestCase\ModuleTestCase;
 use Fi1a\UserSettings\Field;
+use Fi1a\UserSettings\Tab;
+use Fi1a\UserSettings\TabMapper;
 
 /**
  * Тестирование полей
@@ -13,11 +15,80 @@ use Fi1a\UserSettings\Field;
 class FieldTest extends ModuleTestCase
 {
     /**
+     * @var int[]
+     */
+    private static $tabIds = [];
+
+    /**
+     * @inheritDoc
+     */
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        $tab = Tab::create([
+            'ACTIVE' => 1,
+            'CODE' => 'FUS_TEST_TAB1',
+            'LOCALIZATION' => null,
+        ]);
+        $result = $tab->add();
+        if (!$result->isSuccess()) {
+            throw new \ErrorException();
+        }
+        self::$tabIds['FUS_TEST_TAB1'] = $result->getId();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function tearDownAfterClass(): void
+    {
+        foreach (self::$tabIds as $tabId) {
+            TabMapper::getById($tabId)->delete();
+        }
+        parent::tearDownAfterClass();
+    }
+
+    /**
      * Тестирование фабричного метода
      */
     public function testCreate(): void
     {
         $field = Field::create(['ID' => 1]);
         $this->assertInstanceOf(Field::class, $field);
+    }
+
+    /**
+     * Добавление
+     *
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     * @throws \Bitrix\Main\SystemException
+     */
+    public function testAdd(): void
+    {
+        $field = Field::create([
+            'TAB_ID' => self::$tabIds['FUS_TEST_TAB1'],
+            'ACTIVE' => 1,
+            'UF' => [
+                'FIELD_NAME' => 'UF_FIELD1',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => '',
+                'SORT' => '500',
+                'MULTIPLE' => 'N',
+                'MANDATORY' => 'Y',
+                'SETTINGS' => [
+                    'DEFAULT_VALUE' => '',
+                    'SIZE' => '20',
+                    'ROWS' => '1',
+                    'MIN_LENGTH' => '0',
+                    'MAX_LENGTH' => '0',
+                    'REGEXP' => '',
+                ],
+                'EDIT_FORM_LABEL' => ['ru' => '', 'en' => '',],
+                'ERROR_MESSAGE' => null,
+                'HELP_MESSAGE' => ['ru' => '', 'en' => '',],
+            ],
+        ]);
+        $this->assertTrue($field->add()->isSuccess());
     }
 }
