111db85d7464e94a9a907cd9f754d10472074012
Nikolay Gorohovsky <fi1a@icloud.com>
2020-12-05 11:54:42 +0500

    Тестирование метода getById класса TabMapper

--- tests/TabMapperTest.php
+++ tests/TabMapperTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Fi1a\Unit\UserSettings;
 
 use Fi1a\Unit\UserSettings\TestCase\TabsAndFieldsTestCase;
+use Fi1a\UserSettings\ITab;
 use Fi1a\UserSettings\TabCollection;
 use Fi1a\UserSettings\TabMapper;
 
@@ -29,5 +30,19 @@ class TabMapperTest extends TabsAndFieldsTestCase
         ]);
         $this->assertInstanceOf(TabCollection::class, $collection);
         $this->assertCount(1, $collection);
+    }
+
+    /**
+     * Тестирование getById
+     *
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     * @throws \Bitrix\Main\SystemException
+     */
+    public function testGetById(): void
+    {
+        $tab = TabMapper::getById(self::$tabIds['FUS_TEST_TAB1']);
+        $this->assertInstanceOf(ITab::class, $tab);
+        $this->assertEquals(self::$tabIds['FUS_TEST_TAB1'], $tab['ID']);
     }
 }
